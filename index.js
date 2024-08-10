e41fd10b1d87b76e4d2cc8e1ed2ac74004b07ad1
Jacob <jacobmrobison472@gmail.com>
2024-08-10 19:17:08 -0400

    second attempt at prompting

--- index.js
+++ index.js
@@ -1,39 +1,80 @@
 const inquirer = require('inquirer');
 
-// Your code for creating SVG logos goes here
 
-// Example usage of inquirer
+
 inquirer.prompt([
 
 
-const questions = [
-    {
-type: 'input',
-name: 'text',
-message: 'Enter the text for the logo:',
-    },
+        {
+title: 'What shape would you like to draw?',
+type: 'list',
+name: 'shape',
+choices: ['Square', 'Circle', 'Triangle']
+}, {    
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+        },{
+        title: 'What should be the size of the shape?',
+        type: 'number',
+        name: 'size',
+        default: 100
+        },{
+        title: 'What should be the color of the shape?',
+        type: 'input',
+        name: 'color',
+        default: 'red'
+        },{
+        title: 'What should be the background color of the shape?',
+        type: 'input',
+        name: 'bgColor',
+        default: 'white'
+        },{
+        title: 'What should be the border radius of the shape?',
+        type: 'number',
+        name: 'borderRadius',
+        default: 0
+        },{
+
+        title: 'What should be the border width of the shape?',
+        type: 'number',
+        name: 'borderWidth',
+        default: 0
+        },{
+        title: 'What should be the border color of the shape?',
+        type: 'input',
+        name: 'borderColor',
+        default: 'black'
+        },{
+        title: 'What should be the text inside the shape?',
+        type: 'input',
+        name: 'text',
+        default: ''
+        },{
+        title: 'What should be the text color of the shape?',
+        type: 'input',
+        name: 'textColor',
+        default: 'black'
+        }
+
+    ])
+    
+
+    
+
+
 
 
 
     
     .then((answers) => {
         // Handle user input
-        console.log(answers);
-
-
-
-        // Your code for creating SVG logos goes here
-        const svg = `
-        <svg width="${answers.size}" height="${answers.size}" xmlns="http://www.w3.org/2000/svg">
-        <rect width="${answers.size}" height="${answers.size}" fill="${answers.color}" rx="${answers.borderRadius}" ry="${answers.borderRadius}" stroke="${answers.borderColor}" stroke-width="${answers.borderWidth}"/>
-        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="${answers.textAlign}" fill="${answers.textColor}" font-size="${answers.fontSize}" font-family="${answers.fontFamily}" text-decoration="${answers.textDecoration}" text-transform="${answers.textTransform}" style="text-shadow: ${answers.textShadowColor} ${answers.textShadowBlur}px ${answers.textShadowBlur}px ${answers.textShadowBlur}px;">${answers.text}</text>
-        </svg>
-        `;
-        console.log(svg);
-
-
-    })
+        console.log(answers)
+    }
+    )
     .catch((error) => {
         // Handle errors
-        console.log(error);
-    });
+        console.log(error)
+    }
+    );
+
