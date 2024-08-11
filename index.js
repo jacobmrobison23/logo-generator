a1cfc56ebefe20444add20eb9e836a73ecc104bd
Jacob <jacobmrobison472@gmail.com>
2024-08-10 21:25:30 -0400

    minor additions to index

--- index.js
+++ index.js
@@ -1,58 +1,78 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateShape = require("./utils/generateShape");
 
-inquirer.prompt([
-  {
-    title: "What shape would you like to draw?",
-    type: "list",
-    name: "shape",
-    choices: ["Square", "Circle", "Triangle"],
-  },
-  {
-    title: "What should be the size of the shape?",
-    type: "number",
-    name: "size",
-    default: 100,
-  },
-  {
-    title: "What should be the color of the shape?",
-    type: "input",
-    name: "color",
-    default: "red",
-  },
-  {
-    title: "What should be the background color of the shape?",
-    type: "input",
-    name: "bgColor",
-    default: "white",
-  },
-  {
-    title: "What should be the border radius of the shape?",
-    type: "number",
-    name: "borderRadius",
-    default: 0,
-  },
-  {
-    title: "What should be the border width of the shape?",
-    type: "number",
-    name: "borderWidth",
-    default: 0,
-  },
-  {
-    title: "What should be the border color of the shape?",
-    type: "input",
-    name: "borderColor",
-    default: "black",
-  },
-  {
-    title: "What should be the text inside the shape?",
-    type: "input",
-    name: "text",
-    default: "",
-  },
-  {
-    title: "What should be the text color of the shape?",
-    type: "input",
-    name: "textColor",
-    default: "black",
-  },
-]);
+
+
+
+
+
+  const questions = [
+    {
+      title: "What shape would you like to draw?",
+      type: "list",
+      name: "shape",
+      choices: ["Square", "Circle", "Triangle"],
+    },
+    {
+      title: "What should be the size of the shape?",
+      type: "number",
+      name: "size",
+      default: 100,
+    },
+    {
+      title: "What should be the color of the shape?",
+      type: "input",
+      name: "color",
+      default: "red",
+    },
+    {
+      title: "What should be the background color of the shape?",
+      type: "input",
+      name: "bgColor",
+      default: "white",
+    },
+    {
+      title: "What should be the border radius of the shape?",
+      type: "number",
+      name: "borderRadius",
+      default: 0,
+    },
+    {
+      title: "What should be the border width of the shape?",
+      type: "number",
+      name: "borderWidth",
+      default: 0,
+    },
+    {
+      title: "What should be the border color of the shape?",
+      type: "input",
+      name: "borderColor",
+      default: "black",
+    },
+    {
+      title: "What should be the text inside the shape?",
+      type: "input",
+      name: "text",
+      default: "",
+    },
+    {
+      title: "What should be the text color of the shape?",
+      type: "input",
+      name: "textColor",
+      default: "black",
+    },
+  ];
+
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const shape = generateShape(answers);
+    const filePath = path.resolve(__dirname, "shape.html");
+    fs.writeFileSync(filePath, shape)
+    console.log("Shape generated at", filePath);
+  }
+    );
+}
+
+init();
